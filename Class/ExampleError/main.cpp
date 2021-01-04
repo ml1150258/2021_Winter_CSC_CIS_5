58aaabb9b09a93c4da6364ac4617b374c826216c
Mark Lehr <mark.lehr@rcc.edu>
2021-01-04 11:32:29 -0800

    Signed-off-by: Mark Lehr <mark.lehr@rcc.edu>

---
+++ Class/ExampleError/main.cpp
@@ -0,0 +1,38 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on January 4, 2021, 10:50 AM
+ * Purpose:  To show computers can't add/subtract
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    short a,b,c;
+    
+    //Initialize Variables
+    a=30000;
+    b=4000;
+    
+    //Map Inputs to Outputs -> Process
+    c=a+b;
+    
+    //Display Inputs/Outputs
+    cout<<c<<" = "<<a<<" + "<<b<<endl;
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on January 4, 2021, 10:50 AM
+ * Purpose:  First Program "Hello World"
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+//Math, Science, Universal, Conversions, High Dimensioned Arrays
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Initialize the Random Number Seed
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map Inputs to Outputs -> Process
+    
+    //Display Inputs/Outputs
+    cout<<"Hello World"<<endl;
+    
+    //Exit the Program - Cleanup
+    return 0;
+}
